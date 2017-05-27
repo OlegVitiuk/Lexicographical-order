89c0aca446fc2bd5a8ab3018b3ff9ed3b20561d9
Oleg <vitiukoleh@gmail.com>
2017-05-27 13:44:58 +0300

    complete App model. Problem with importing a checkcombox

--- src/sample/AppModel.java
+++ src/sample/AppModel.java
@@ -3,20 +3,42 @@ package sample;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
+import java.util.ArrayList;
+
 /**
  * Created by BusinessPC on 5/27/2017.
  */
 public class AppModel {
     private final IntegerProperty amountOfVariables  = new SimpleIntegerProperty();
+    private final IntegerProperty amountOfRows  = new SimpleIntegerProperty();
+    private final ArrayList<Integer> arrayWithValuesOfX  = new ArrayList<Integer>();
+
 
     public IntegerProperty amountOfVariablesProperty(){
         return amountOfVariables;
     }
+    public IntegerProperty amountOfRowsProperty(){
+        return amountOfRows;
+    }
+
+
     public final Integer getAmountOfVariables(){
         return amountOfVariablesProperty().get();
     }
     public final void setAmountOfVariables(Integer amountOfVariables){
         amountOfVariablesProperty().set(amountOfVariables);
     }
+    public final Integer getAmountOfRows(){
+        return amountOfRowsProperty().get();
+    }
+    public final void setAmountOfRows(Integer amountOfRows){
+        amountOfRowsProperty().set(amountOfRows);
+    }
 
+    public final ArrayList getArrayWithValuesOfX(){
+        return arrayWithValuesOfX;
+    }
+    public final void setArrayWithValuesOfX(int value){
+        this.arrayWithValuesOfX.add(value);
+    }
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,9 @@
 package sample;
 
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+//import controlsfx.control.CheckComboBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,6 +13,8 @@ public class Controller{
     private AppModel model;
 
     @FXML private ComboBox amountOfVariables;
+    @FXML private ComboBox amountOfRows;
+    @FXML private ComboBox xValues;
 
     public Controller(AppModel model){
         this.model = model;
@@ -32,5 +34,11 @@ public class Controller{
     }
     @FXML private void sentAmountOfVariables(){
         model.setAmountOfVariables(Integer.valueOf(amountOfVariables.getSelectionModel().getSelectedItem().toString()));
+    }
+    @FXML private void sentAmountOfRows(){
+        model.setAmountOfRows(Integer.valueOf(amountOfRows.getSelectionModel().getSelectedItem().toString()));
+    }
+    @FXML private void pushXValues(){
+        //model.setArrayWithValuesOfX(xValues.getSelectionModel().get);
     }
 }
